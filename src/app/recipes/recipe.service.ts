75300b43c5c3719c5d674b344d8a1120042add83
MolnarArthur <molnararthur26@gmail.com>
2018-06-06 22:46:33 +0300

    Adding Ingredients to Shopping List

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,16 +1,38 @@
 import { Recipe } from './recipe.model'
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+
+@Injectable()
 export class RecipeService {
     
     recipeSelected = new EventEmitter<Recipe>()
 
     private recipes: Recipe[] = [
-        new Recipe('Title', 'Description', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-        new Recipe('Other', 'OtherDescription', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+        new Recipe(
+            'First Item', 'This is the first description', 
+            'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg', 
+            [
+                new Ingredient('Rice', 2),
+                new Ingredient('French Fries', 20)
+            ]),
+        new Recipe('Second Item', 
+        'Test description!', 
+        'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg', 
+        [
+            new Ingredient('Tomato', 2),
+            new Ingredient('Grapes', 12)
+        ])
       ]
     
+      constructor(private slService: ShoppingListService) { }
+
       getRecipes() {
           return this.recipes.slice()
+      }
+
+      addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.slService.addIngredients(ingredients)
       }
 }

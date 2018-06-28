0dc1b68ef1db2fa502fae194c44becb82bf6c913
MolnarArthur <molnararthur26@gmail.com>
2018-06-28 19:01:28 +0300

    Logout with Firebase

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,11 +2,15 @@ import { Injectable } from '@angular/core'
 import { Http, Response } from '@angular/http'
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model'
+import { AuthService } from '../auth/auth.service';
+
 
 
 @Injectable()
 export class DataStorageService {
-    constructor(private http: Http, private recipeService: RecipeService) {
+    constructor(private http: Http, 
+                private recipeService: RecipeService,
+                private authService: AuthService) {
 
     }
 
@@ -15,7 +19,9 @@ export class DataStorageService {
     }
 
     getRecipes() {
-        this.http.get('https://ng-recipe-book-8e1db.firebaseio.com/recipes.json')
+        const token = this.authService.getToken()
+
+        this.http.get('https://ng-recipe-book-8e1db.firebaseio.com/recipes.json?auth=' + token)
           .subscribe(
               (response: Response) => {
                   const recipes: Recipe[] = response.json()

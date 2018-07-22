9911d7d9ead3295b1c6b6109638704700f2f52e9
MolnarArthur <molnararthur26@gmail.com>
2018-07-22 16:49:47 +0300

    Handle user Actions using the Store

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpEvent } from '@angular/common/http'
+// import { HttpEvent } from '@angular/common/http'
 import { DataStorageService } from '../../shared/data-storage.service';
 import { AuthService } from '../../auth/auth.service';
+import { Store } from '@ngrx/store';
+import * as fromApp from '../../store/app.reducers'
+import * as fromAuth from '../../auth/store/auth.reducers'
+import { Observable } from 'rxjs/Observable'
 
 @Component({
   selector: 'app-header',
@@ -10,19 +14,23 @@ import { AuthService } from '../../auth/auth.service';
 })
 export class HeaderComponent implements OnInit {
 
+  authState: Observable<fromAuth.State>
+
   constructor(private dataStorageService: DataStorageService,
-              private authService: AuthService) { }
+              private authService: AuthService,
+              private store: Store<fromApp.AppState>) { }
 
   ngOnInit() {
+    this.authState = this.store.select('auth')
   }
 
   onSaveData() {
-    this.dataStorageService.storeRecipes()
-    .subscribe(
-      (response: HttpEvent<Object>) => {
-        console.log(response)
-      }
-    )
+    // this.dataStorageService.storeRecipes()
+    // .subscribe(
+    //   (response: HttpEvent<Object>) => {
+    //     console.log(response)
+    //   }
+    // )
   }
 
   onFetchData() {
--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -4,8 +4,8 @@ import { Subscription } from 'rxjs';
 import { Store } from '@ngrx/store';
 
 import { Ingredient } from '../../shared/ingredient.model'
+import * as fromApp from '../../store/app.reducers'
 import * as ShoppingListActions from '../store/shopping-list.actions'
-import * as fromShoppingList from '../store/shopping-list.reducers'
 
 @Component({
   selector: 'app-shopping-edit',
@@ -20,7 +20,7 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
   editMode = false
   editedItem: Ingredient
 
-  constructor(private store: Store<fromShoppingList.AppState>) { }
+  constructor(private store: Store<fromApp.AppState>) { }
 
   ngOnInit() {
     this.subscription = this.store.select('shoppingList')
